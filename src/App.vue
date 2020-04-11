df048b451295e9841fe160b70121ebd7433dc4a0
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-10 20:56:11 -0700

    Add header to top of page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/profile">Profile</router-link>
-    </div>
+    <Header />
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header">
+    <h1>Tab Finder</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/profile">Profile</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+</style>
