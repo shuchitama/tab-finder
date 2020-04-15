bb8473b019d48d80779a106a68743c8d22fddc69
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-15 15:22:08 -0700

    recieve submit event and pass payload to songlist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
     <Header />
-    
-    <Songlist />
+    <SideBar v-on:chords-submitted="passChordsToSongList"/>
+    <Songlist v-bind:sendToSongList="sendToSongList"/>
   </div>
 </template>
 
 <script>
 import Songlist from "@/components/Songlist.vue";
+import SideBar from "@/components/SideBar.vue";
 
 export default {
   name: "Home",
   components: {
-    Songlist
+    Songlist,
+    SideBar
+  },
+  data () {
+    return {
+      sendToSongList: []
+    }
+  },
+  methods: {
+    passChordsToSongList(chordIDArr) {
+      this.sendToSongList = chordIDArr
+    }
   }
 };
 </script>
