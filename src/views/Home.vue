c200cfb963ae57720c63109ab3520a697458e94e
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-14 18:04:33 -0700

    remove helloworld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
     <Header />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    
     <Songlist />
   </div>
 </template>
 
 <script>
-// import Header from "@/components/Header.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
 import Songlist from "@/components/Songlist.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     Songlist
   }
 };
