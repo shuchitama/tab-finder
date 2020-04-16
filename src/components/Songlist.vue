9cf4b004fb43e1625e82e568da9254276d3afb9d
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-15 18:11:59 -0700

    copy helper functions straight into file

--- src/components/Songlist.vue
+++ src/components/Songlist.vue
@@ -1,29 +1,53 @@
 <template>
   <table class="songlist">
-    <header> Your results: </header>
-    <Song v-bind:songs="songs" />
+    <header>Your results:</header>
+    <Song v-bind:filteredSongs="filteredSongs" />
+    <div>from SongList:</div>
   </table>
 </template>
 
 <script>
 import axios from "axios";
 import Song from "./Song.vue";
+import _ from 'lodash'
 export default {
   name: "songlist",
   components: {
     Song
   },
+  props: ["chordIDs"],
   data() {
     return {
-      songs: [],
+      AllChords:[],
+      AllSongs: [],
+      filteredSongs: []
     };
   },
   created() {
-    axios.get("http://localhost:3001/api/songs")
-    .then(res => {
-      console.log(res);
-      this.songs = res.data;
-    });
+    axios.get("http://localhost:3001/api/songs").then(res => {
+      this.AllSongs = res.data;
+    }),
+    axios.get("http://localhost:3001/api/chords").then(res => {
+      this.AllChords = res.data;
+    })
+  },
+  methods: {
+    chordIDtoName(chordList, arrOfIDs) {
+    return arrOfIDs.map (id => {
+      const chord = chordList.find(element => element.id === id);
+      return chord.name;
+      })
+    },
+    filtersongs() {
+      console.log("fired!")
+      const arrOfNames = this.chordIDtoName(this.AllChords, this.$props.chordIDs)
+      this.filteredSongs = this.AllSongs.filter(song => _.isEqual(song.chords.sort(), arrOfNames.sort()))
+    }
+    // searchResults() {
+    //   this.filteredSongs = [1, 2, 3]
+    //   filtersongs(this.AllSongs, this.$props.chordIDs);
+    //   console.log(this.filteredSongs)
+    // }
   }
 };
 </script>
