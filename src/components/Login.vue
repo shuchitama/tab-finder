fb23064bd54b922c1e88e4280db33043e3de9618
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-16 20:30:50 -0700

    remove unused code

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -13,37 +13,23 @@
       ref="modal"
       title="Login"
       size="sm"
-      @show="resetModal"
-      @hidden="resetModal"
-      @ok="handleOk"
     >
 
       <form ref="form" @submit.stop.prevent="handleSubmit">
         <b-form-group
-          :state="nameState"
           label="Name"
-          label-for="name-input"
-          invalid-feedback="Name is required"
         >
           <b-form-input
             id="name-input"
-            v-model="name"
-            :state="nameState"
-            required
           ></b-form-input>
         </b-form-group>
           <b-form-group
-          :state="passwordState"
           label="Password"
           label-for="password-input"
-          invalid-feedback="Password is required"
         >
           <b-form-input
             id="password-input"
             type="password"
-            v-model="password"
-            :state="passwordState"
-            required
           ></b-form-input>
         </b-form-group>
       </form>
@@ -77,34 +63,8 @@
       },
       logout() {
         this.loggedOut = true;
-      },
-      checkFormValidity() {
-        const valid = this.$refs.form.checkValidity()
-        this.nameState = valid
-        return valid
-      },
-      resetModal() {
-        this.name = ''
-        this.nameState = null
-      },
-      handleOk(bvModalEvt) {
-        // Prevent modal from closing
-        bvModalEvt.preventDefault()
-        // Trigger submit handler
-        this.handleSubmit()
-      },
-      handleSubmit() {
-        // Exit when the form isn't valid
-        if (!this.checkFormValidity()) {
-          return
-        }
-        // Push the name to submitted names
-        this.submittedNames.push(this.name)
-        // Hide the modal manually
-        this.$nextTick(() => {
-          this.$bvModal.hide('modal-prevent-closing')
-        })
       }
+      
     }
   }
 </script>
