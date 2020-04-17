0df5c53a4312481a891660735d3fc311de55afda
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-16 18:17:07 -0700

    pull out login into a separate component

---
+++ src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+<a>
+  <div>
+    <b class="login" v-b-modal.modal-prevent-closing>Login</b>
+      to create a list with your favourite songs
+  </div>
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Login"
+      size="sm"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          :state="nameState"
+          label="Name"
+          label-for="name-input"
+          invalid-feedback="Name is required"
+        >
+          <b-form-input
+            id="name-input"
+            v-model="name"
+            :state="nameState"
+            required
+          ></b-form-input>
+        </b-form-group>
+          <b-form-group
+          :state="passwordState"
+          label="Password"
+          label-for="password-input"
+          invalid-feedback="Password is required"
+        >
+          <b-form-input
+            id="password-input"
+            type="password"
+            v-model="password"
+            :state="passwordState"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </form>
+
+      <template v-slot:modal-footer>
+        <b-button
+          variant="success"
+          class="float-right"
+          size="sm"
+          @click="show=false"
+        >
+          Login
+        </b-button>
+      </template>
+
+    </b-modal>
+  </a>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,59 +3,7 @@
     <Header />
     <SideBar v-on:chords-submitted="passChordsToSongList"/>
     <br />
-    <div>
-      <b class="login" v-b-modal.modal-prevent-closing>Login</b>
-       to create a list with your favourite songs
-    </div>
-    <b-modal
-      id="modal-prevent-closing"
-      ref="modal"
-      title="Login"
-      size="sm"
-      @show="resetModal"
-      @hidden="resetModal"
-      @ok="handleOk"
-    >
-      <form ref="form" @submit.stop.prevent="handleSubmit">
-        <b-form-group
-          :state="nameState"
-          label="Name"
-          label-for="name-input"
-          invalid-feedback="Name is required"
-        >
-          <b-form-input
-            id="name-input"
-            v-model="name"
-            :state="nameState"
-            required
-          ></b-form-input>
-        </b-form-group>
-          <b-form-group
-          :state="passwordState"
-          label="Password"
-          label-for="password-input"
-          invalid-feedback="Password is required"
-        >
-          <b-form-input
-            id="password-input"
-            type="password"
-            v-model="password"
-            :state="passwordState"
-            required
-          ></b-form-input>
-        </b-form-group>
-      </form>
-      <template v-slot:modal-footer>
-        <b-button
-          variant="success"
-          class="float-right"
-          size="sm"
-          @click="show=false"
-        >
-          Login
-        </b-button>
-      </template>
-    </b-modal>
+    <Login />
 
     <br />
     <Songlist v-bind:chordIDs="chordIDs"/>
@@ -65,12 +13,14 @@
 <script>
 import Songlist from "@/components/Songlist.vue";
 import SideBar from "@/components/SideBar.vue";
+import Login from "@/components/Login.vue";
 
 export default {
   name: "Home",
   components: {
     Songlist,
-    SideBar
+    SideBar,
+    Login
   },
   data () {
     return {
