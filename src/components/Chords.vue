39d7b830d37eacf8cc18288b356d8eee8a427b36
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-30 20:18:02 -0700

    move data, methods from Chords component to SideBar

--- src/components/Chords.vue
+++ src/components/Chords.vue
@@ -30,61 +30,61 @@
 export default {
   name: "Chords",
   props: ["chords"],
-  data() {
-    return {
-      selectChords: [
-        { id: 1, selected: false },
-        { id: 2, selected: false },
-        { id: 3, selected: false },
-        { id: 4, selected: false },
-        { id: 5, selected: false },
-        { id: 6, selected: false },
-        { id: 7, selected: false },
-        { id: 8, selected: false },
-        { id: 9, selected: false },
-        { id: 10, selected: false },
-        { id: 11, selected: false },
-        { id: 12, selected: false },
-        { id: 13, selected: false },
-        { id: 14, selected: false },
-        { id: 15, selected: false },
-        { id: 16, selected: false },
-        { id: 17, selected: false },
-        { id: 18, selected: false },
-        { id: 19, selected: false },
-        { id: 20, selected: false },
-        { id: 21, selected: false },
-        { id: 22, selected: false },
-        { id: 23, selected: false },
-        { id: 24, selected: false }
-      ]
-    };
-  },
+  // data() {
+  //   return {
+  //     selectChords: [
+  //       { id: 1, selected: false },
+  //       { id: 2, selected: false },
+  //       { id: 3, selected: false },
+  //       { id: 4, selected: false },
+  //       { id: 5, selected: false },
+  //       { id: 6, selected: false },
+  //       { id: 7, selected: false },
+  //       { id: 8, selected: false },
+  //       { id: 9, selected: false },
+  //       { id: 10, selected: false },
+  //       { id: 11, selected: false },
+  //       { id: 12, selected: false },
+  //       { id: 13, selected: false },
+  //       { id: 14, selected: false },
+  //       { id: 15, selected: false },
+  //       { id: 16, selected: false },
+  //       { id: 17, selected: false },
+  //       { id: 18, selected: false },
+  //       { id: 19, selected: false },
+  //       { id: 20, selected: false },
+  //       { id: 21, selected: false },
+  //       { id: 22, selected: false },
+  //       { id: 23, selected: false },
+  //       { id: 24, selected: false }
+  //     ]
+  //   };
+  // },
   methods: {
-    addChord(id) {
-      const chord = this.selectChords.find(element => element.id === id);
-      chord.selected = !chord.selected;
-    },
-    showChords() {
-      const selectedChordsObj = this.selectChords.filter(
-        element => element.selected === true
-      );
-      const selectedChordIDs = [];
-      selectedChordsObj.map(element => selectedChordIDs.push(element.id));
-      return selectedChordIDs;
-    },
-    isActive(id) {
-      const chord = this.selectChords.find(element => element.id === id);
-      return chord.selected;
-    },
-    submitChords() {
-      this.$emit("chords-submitted", this.showChords());
-    },
-    clearChords() {
-      this.selectChords.forEach(element => {
-        element.selected = false;
-      });
-    }
+    // addChord(id) {
+    //   const chord = this.selectChords.find(element => element.id === id);
+    //   chord.selected = !chord.selected;
+    // },
+    // showChords() {
+    //   const selectedChordsObj = this.selectChords.filter(
+    //     element => element.selected === true
+    //   );
+    //   const selectedChordIDs = [];
+    //   selectedChordsObj.map(element => selectedChordIDs.push(element.id));
+    //   return selectedChordIDs;
+    // },
+    // isActive(id) {
+    //   const chord = this.selectChords.find(element => element.id === id);
+    //   return chord.selected;
+    // },
+    // submitChords() {
+    //   this.$emit("chords-submitted", this.showChords());
+    // },
+    // clearChords() {
+    //   this.selectChords.forEach(element => {
+    //     element.selected = false;
+    //   });
+    // }
   }
 };
 </script>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -14,7 +14,33 @@ export default {
   },
   data() {
     return {
-      chords: []
+      chords: [],
+      selectChords: [
+        { id: 1, selected: false },
+        { id: 2, selected: false },
+        { id: 3, selected: false },
+        { id: 4, selected: false },
+        { id: 5, selected: false },
+        { id: 6, selected: false },
+        { id: 7, selected: false },
+        { id: 8, selected: false },
+        { id: 9, selected: false },
+        { id: 10, selected: false },
+        { id: 11, selected: false },
+        { id: 12, selected: false },
+        { id: 13, selected: false },
+        { id: 14, selected: false },
+        { id: 15, selected: false },
+        { id: 16, selected: false },
+        { id: 17, selected: false },
+        { id: 18, selected: false },
+        { id: 19, selected: false },
+        { id: 20, selected: false },
+        { id: 21, selected: false },
+        { id: 22, selected: false },
+        { id: 23, selected: false },
+        { id: 24, selected: false }
+      ]
     };
   },
   created() {
@@ -25,6 +51,30 @@ export default {
   methods: {
     passToParent(payload) {
       this.$emit("chords-submitted", payload);
+    },
+    addChord(id) {
+      const chord = this.selectChords.find(element => element.id === id);
+      chord.selected = !chord.selected;
+    },
+    showChords() {
+      const selectedChordsObj = this.selectChords.filter(
+        element => element.selected === true
+      );
+      const selectedChordIDs = [];
+      selectedChordsObj.map(element => selectedChordIDs.push(element.id));
+      return selectedChordIDs;
+    },
+    isActive(id) {
+      const chord = this.selectChords.find(element => element.id === id);
+      return chord.selected;
+    },
+    submitChords() {
+      this.$emit("chords-submitted", this.showChords());
+    },
+    clearChords() {
+      this.selectChords.forEach(element => {
+        element.selected = false;
+      });
     }
   }
 };
